588a07f1be102d53700874058071219adc5dde39
Евгений <eugene@localhost.localdomain>
2018-04-15 15:46:18 +0700

    Menu 3

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 int main()
 {
 	int menu = 0;
+	int settings = 0;
 
 	while (1) {
 		menu = main_menu();
@@ -17,7 +18,22 @@ int main()
 		}
 
 		if(menu == 3){
-		
+		    while(menu == 3){
+			    system("clear");
+			    printf("\t\t\t\t   ________\n");
+			    printf("\t\t\t\t   |    | |\n");
+			    printf("\t\t\t\t   |    | |\n");
+			    printf("\t\t\t\t   |    | |\n");
+			    printf("\t\t\t\t   \\    / /\n");
+			    printf("\t\t\t\t    \\__/_/\n");
+			    printf("\t\t\t\t     ____\n");
+			    printf("\t\t\t\t    /  \\ \\\n");
+			    printf("\t\t\t\t    \\__/_/\n");
+			    printf("\n\n\t\t\t\tПРАВИЛА ИГРЫ\n\n\t\t\t\tВ гомоку играют\n\t\t\t\t2 игрока, в поле 9Х9\n\t\t\t\tили 19 Х 19 за Х и О\n\t\t\t\tу каждого из игроков\n\t\t\t\tцель собрать в ряд 5\n\t\t\t\tкрестов или нолей\n\n\t\t\t\t1.Выход в меню\n\t\t\t\t");
+			    scanf("%d", &settings);
+			    if(settings == 1)
+				    menu = settings;
+		    }
 		}
 
 		if(menu == 4){
