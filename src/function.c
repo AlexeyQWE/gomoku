a1b73ab39e3a8766db93c70c047e08c0c9266514
AlexeyQWE <mountain.skier@yandex.ru>
2018-04-18 23:55:02 +0700

    Add realization  of func. fill_gameboard and print_gameboard

--- src/function.c
+++ src/function.c
@@ -28,3 +28,47 @@ int main_menu()
 	}
 	return menu;
 }
+
+void fill_gameboard(int size_board, char gameboard_mass[size_board][size_board])
+{
+	system("clear");// ОЧИЩАЕМ ОБЛАСТЬ ВЫШЕ, В ДАЛЬНЕЙШЕМ, БУДЕТ ВЫГЯДИТЬ ТАК, ЧТО ДОСКА СТАТИЧНА
+			for(int i = 0; i < size_board - 1; i++){// ЗАПОЛНЯЕМ МАССИВ (ИГРОВУЮ ДОСКУ), ТО ЕСТЬ ПРИВОДИМ К СМОТРИБЕЛЬНОМУ ВИДУ
+				for(int j = 0; j < size_board - 1; j++){
+					gameboard_mass[i][j] = '_';
+				}
+			}
+}
+
+void print_gameboard(int size_board, char gameboard_mass[size_board][size_board])
+{
+	system("clear"); 
+				printf("\n\t\t ____   __      _    _      __    _   __ __   __\n");
+				printf("\t\t(  __) /  \\    / \\  / \\    /  \\  ( )_/ / \\ \\_/ /\n");
+				printf("\t\t) (   ( () )  / /\\\\//\\ \\  ( () ) )  _ |   \\_  /\n");
+				printf("\t\t(_)    \\__/  (_)  --  (_)  \\__/  (_) \\_\\   /_/\n");	
+
+				for(int i = 0; i < size_board - 1; i++){// ЗДЕСЬ ВЫВОДИТСЯ САМА ДОСКА
+					printf("\n\t\t");
+					for(int j = 0; j < size_board - 1; j++){
+						if(i == 0){
+							if(j == 0){// ВЫВОДЯТСЯ ОРИЕНТИРОВОЧНЫЕ КООРДИНАТЫ ПО БОКАМ ИГРОВОГО ПОЛЯ ПО ГОРИЗОНТАЛИ
+								printf("   ");
+							}else if(j < 10){
+								printf(" %d ", j);
+							}else if(j == 9){
+								printf("%d ", j);
+							}else{
+								printf(" %d", j);
+							}
+						}else if(j == 0){// ТЕ ЖЕ САМЫЕ КООРДИНАТЫ, ТОЛЬКО ПО ВЕРТИКАЛИ
+							if(i < 10){
+								printf(" %d ", i);
+							}else{
+								printf("%d ", i);
+							}
+						}else{
+							printf("[%c]", gameboard_mass[i][j]);// ВЫВОД САМОЙ ИГРОВОЙ ДОСКИ
+						}
+					}
+				}
+}
