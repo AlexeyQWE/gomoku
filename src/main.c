9f762417602f671e2c6151339c4fc7f864639e41
AlexeyQWE <mountain.skier@yandex.ru>
2018-04-18 23:48:50 +0700

    Add call of func. fill_gameboard and print_gameboard

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "prototypes.h"
+
+#define TABLE_Y 17	// РАЗМЕР ДОСКИ
 
 int main()
 {
 	int menu = 0;
+	int i = 1;
+	char tableGame[TABLE_Y][TABLE_Y];	//Хранение доски и ходов игроков
 
-	while (1) {
+	while (i == 1) {
 		menu = main_menu();
 	
 		if(menu == 1){
-
+			fill_gameboard(TABLE_Y,tableGame);
+			print_gameboard(TABLE_Y,tableGame);
 		}
 
 		if(menu == 2){
@@ -27,6 +33,7 @@ int main()
 		if(menu == 5){
 
 		}
+		i = 0;
 
 	}
 
