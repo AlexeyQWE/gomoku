e6058b7a2ea16a549f09dbaeacefe6e33a4259ee
Alex Vashov <S-total-war@mail.ru>
2018-04-28 17:55:19 +0700

    Исправил файл с прототипами

--- src/prototypes.h
+++ src/prototypes.h
@@ -1,11 +1,11 @@
-#define TABLE_Y 17
+#define TABLE_Y 16
 
 	void input_nubmers_test(int *numbers);
 
 	int main_menu(void);
 
-	void fill_gameboard(int size_board, char gameboard_mass[TABLE_Y][TABLE_Y]);	
+	void fill_gameboard(char gameboard_mass[TABLE_Y][TABLE_Y]);	
 
-	void print_gameboard(int size_board, char gameboard_mass[TABLE_Y][TABLE_Y]);
+	void print_gameboard(char gameboard_mass[TABLE_Y][TABLE_Y]);
 
 	void game_settings(int *menu, int *settings, int *level, int *vibor, int *bot);
