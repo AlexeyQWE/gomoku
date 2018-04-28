6f514bb1cdc0c234ac6d00c595abd26b4f16d049
AlexeyQWE <mountain.skier@yandex.ru>
2018-04-28 16:15:38 +0700

    Add prototypes of func. input_nubmers_test and game_settings

--- src/prototypes.h
+++ src/prototypes.h
@@ -1,9 +1,11 @@
-#include <stdio.h>
-
 #define TABLE_Y 17
+
+	void input_nubmers_test(int *numbers);
 
 	int main_menu(void);
 
 	void fill_gameboard(int size_board, char gameboard_mass[TABLE_Y][TABLE_Y]);	
 
 	void print_gameboard(int size_board, char gameboard_mass[TABLE_Y][TABLE_Y]);
+
+	void game_settings(int *menu, int *settings, int *level, int *vibor, int *bot);
