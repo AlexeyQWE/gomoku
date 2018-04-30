89a0589436361c62e1a473a2689a24b81048616a
Alex Vashov <S-total-war@mail.ru>
2018-04-30 15:31:12 +0700

    Game Man/Man done!

--- src/main.c
+++ src/main.c
@@ -4,24 +4,37 @@
 
 int main()
 {
-	int i = 1, menu = 0, level = 0, vibor = 1, bot = 1;
+	int i = 1, menu = 0, level = 0, choice = 1, bot = 1;
 	int height = 0, widht = 0;
 	char tableGame[TABLE_Y][TABLE_Y];	//Хранение доски и ходов игроков
 
 	while (i == 1) {
-		int settings = 0, winExit = 0;
+		int settings = 0, winExit = 0, winX = 0, winO = 0;
 		menu = main_menu();
 	
 		if(menu == 1){
 			fill_gameboard(tableGame);
 			do{
 				print_gameboard(tableGame);
-				entering_coord(vibor, winExit, &height, &widht, tableGame);
+				if(winX == 1){
+					printf("\n\n\t\t\tХ - ПОБЕДИЛ\a");
+					winExit = 1;
+				}else if(winO == 1){
+					printf("\n\n\t\t\tO - ПОБЕДИЛ");
+					winExit = 1;
+				}
+				entering_coord(choice, winExit, &height, &widht, tableGame);
+				check_to_win(tableGame, choice, widht, height, winExit, &winX, &winO);
+				if(choice == 1){// ЗДЕСЬ ОПРЕДЕЛЯЕТСЯ ОЧЕРЕДНОСТЬ ХОДОВ
+					choice = 0;
+				}else if(choice == 0){
+					choice = 1;
+				}
 			}while(winExit != 1);
 		}
 
 		if(menu == 2){
-			game_settings(&menu, &settings, &level, &vibor, &bot);
+			game_settings(&menu, &settings, &level, &choice, &bot);
 		}
 
 		if(menu == 3){
