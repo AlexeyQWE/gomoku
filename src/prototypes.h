a85cdb996aae41e241bdffb7e175485f71568ed9
Alex Vashov <S-total-war@mail.ru>
2018-05-05 18:20:12 +0700

    Fixed name variables

--- src/prototypes.h
+++ src/prototypes.h
@@ -12,9 +12,9 @@
 
 	void entering_coord(int choice, int winExit, int *height, int *widht, char tableGame[TABLE_Y][TABLE_Y]);
 
-	void check_to_win(char tableGame[TABLE_Y][TABLE_Y], int choice, int widht, int height, int winExit, int *winX, int *winO, int *gorizontSchet, int *vertikalSchet, int *leftDiagonalSchet, int *rightDiagonalSchet, int *gorizontSchetLeft, int *vertikalSchetLeft, int *leftDiagonalSchetLeft, int *rightDiagonalSchetLeft);
+	void check_to_win(char tableGame[TABLE_Y][TABLE_Y], int choice, int widht, int height, int winExit, int *winX, int *winO, int *gorizontScore, int *vertikalScore, int *leftDiagonalScore, int *rightDiagonalScore, int *gorizontScoreLeft, int *vertikalScoreLeft, int *leftDiagonalScoreLeft, int *rightDiagonalScoreLeft);
 
-	void move_bot(int level, int gorizontSchet, int vertikalSchet, int leftDiagonalSchet, int rightDiagonalSchet, int gorizontSchetLeft, int vertikalSchetLeft, int leftDiagonalSchetLeft, int rightDiagonalSchetLeft, int hodBot);
+	void move_bot(int level, int gorizontScore, int vertikalScore, int leftDiagonalScore, int rightDiagonalScore, int gorizontScoreLeft, int vertikalScoreLeft, int leftDiagonalScoreLeft, int rightDiagonalScoreLeft, int hodBot);
 
 	void control(int exit, int i, int choice, char tableGame[TABLE_Y][TABLE_Y], int coord, int *score, int *spaceFinder, int hightCoordLeft, int hightCoordRight, int weightCoordLeft, int weightCoordRight, int *check, int checkTwo);
 
