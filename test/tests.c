cbf690d985b59625195905e706cab043c47cbfa1
AlexeyQWE <mountain.skier@yandex.ru>
2018-05-10 18:06:38 +0700

    tests for function fill_and_print_gameboard

--- test/tests.c
+++ test/tests.c
@@ -11,3 +11,21 @@ CTEST(testing_suite, correct_entering)
 	ASSERT_INTERVAL(0, 5, result); //Проверка на то, что из функции вернется число от 0 до 5
 }
 
+CTEST(testing_suite, fill_and_print_gameboard)
+{
+	//GIVEN
+	char tableGame [16][16];
+
+	//WHEN
+	const int result1 = fill_gameboard(tableGame);
+	const int result2 = print_gameboard(tableGame);
+
+	//Проверка на то, что из функции вернется 0(в случае выхода за границы масиива вернется NULL и тест будет провален)
+
+	//THEN
+	const int expected1 = 0;
+	ASSERT_EQUAL(expected1, result1);
+	const int expected2 = 0;
+	printf("\n");
+	ASSERT_EQUAL(expected2, result2);
+}
