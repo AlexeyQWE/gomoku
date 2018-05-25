5be8e148d72d563b04ee80517aa3db64555a54b3
AlexeyQWE <mountain.skier@yandex.ru>
2018-05-25 21:19:13 +0700

    Correct formatting code

--- src/prototypes.h
+++ src/prototypes.h
@@ -11,14 +11,14 @@
 /*ANSI/VT100 Terminal using example */
 
 
-#define home() 			printf(ESC "[H") //Move cursor to the indicated row, column (origin at 1,1)
-#define clrscr()		printf(ESC "[2J") //lear the screen, move to (1,1)
-#define gotoxy(x,y)		printf(ESC "[%d;%dH", y, x);
+#define home() 			 printf(ESC "[H") //Move cursor to the indicated row, column (origin at 1,1)
+#define clrscr()		 printf(ESC "[2J") //lear the screen, move to (1,1)
+#define gotoxy(x,y)		 printf(ESC "[%d;%dH", y, x);
 #define visible_cursor() printf(ESC "[?251");
 /* 
 Set Display Attribute Mode	<ESC>[{attr1};...;{attrn}m
 */
-#define resetcolor() printf(ESC "[0m")
+#define resetcolor()				printf(ESC "[0m")
 #define set_display_atrib(color) 	printf(ESC "[%dm",color)
 
 #define TABLE_Y 16
