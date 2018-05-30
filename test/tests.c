0fcc8137b0d2eb891fba6ff006e67792e19b3b1e
AlexeyQWE <mountain.skier@yandex.ru>
2018-05-30 21:34:59 +0700

    test for func. step_attack_bot_diagonal_plus

--- test/tests.c
+++ test/tests.c
@@ -531,3 +531,29 @@ CTEST(testing_suite,step_attack_bot_vertical)
 	ASSERT_EQUAL(5, height);
 	ASSERT_EQUAL(exit, i);
 }
+
+CTEST(testing_suite, step_attack_bot_diagonal_plus) 
+{
+	//GIVEN
+	char tableGame[16][16];
+	for(int i = 0; i < TABLE_Y; i++){
+		for(int j = 0; j < TABLE_Y; j++){
+			tableGame[i][j] = '_';
+		}
+	}
+	tableGame[6][6] = 'X';
+
+	int choice = 1, hodBot = 0, height = 6, widht = 6, exit = 11, i = 7, j = 1;
+
+	//WHEN
+	const int result = step_attack_bot_diagonal_plus(j, choice, tableGame, &i, &widht, &hodBot, exit, &height);
+
+	//THEN
+	const int expected = 1;
+	const int expected1 = 0;
+	ASSERT_EQUAL(expected1, result);
+	ASSERT_EQUAL(expected, hodBot);
+	ASSERT_EQUAL(7, height);
+	ASSERT_EQUAL(7, widht);
+	ASSERT_EQUAL(exit, i);
+}
